8fc17fe25ef006b79a1a98a5868bacede0b603c1
jaredianmills <jaredianmills@gmail.com>
2018-08-21 11:26:31 -0400

    created controllers for users and scores

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::ScoresController < ApplicationController
-  belongs_to :user
+
+  before_action :set_score, only: [:show]
+
+  def index
+    scores = Score.all
+    render json: scores, status: 200
+  end
+
+  def create
+    score = Score.create(score_params)
+    render json: score, status: 201
+  end
+
+  def show
+    render json: @score, status: 200
+  end
+
+  private
+  def score_params
+    params.permit(:total, :user_id)
+  end
+
+  def set_score
+    @score = Score.find(params[:id])
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,26 @@
 class Api::V1::UsersController < ApplicationController
-  has_many :scores
+  before_action :set_user, only: [:show]
+
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user, status: 201
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+
+  private
+  def user_params
+    params.permit(:name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
